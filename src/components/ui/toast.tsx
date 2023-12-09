12a00157f6f6b1d2121e619fc9a9015b9bac4dcd
Akshai-M <mylapuramakshaikumar@gmail.com>
2023-12-09 23:05:09 +0530

    modified

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -100,21 +100,21 @@ const ToastTitle = React.forwardRef<
 ))
 ToastTitle.displayName = ToastPrimitives.Title.displayName
 
-// const ToastDescription = React.forwardRef<
-//   React.ElementRef<typeof ToastPrimitives.Description>,
-//   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Description>
-// >(({ className, ...props }, ref) => (
-//   <ToastPrimitives.Description
-//     ref={ref}
-//     className={cn("text-sm opacity-90", className)}
-//     {...props}
-//   />
-// ))
-// ToastDescription.displayName = ToastPrimitives.Description.displayName
+const ToastDescription = React.forwardRef<
+  React.ElementRef<typeof ToastPrimitives.Description>,
+  React.ComponentPropsWithoutRef<typeof ToastPrimitives.Description>
+>(({ className, ...props }, ref) => (
+  <ToastPrimitives.Description
+    ref={ref}
+    className={cn("text-sm opacity-90", className)}
+    {...props}
+  />
+))
+ToastDescription.displayName = ToastPrimitives.Description.displayName
 
-// type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
+type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>
 
-// type ToastActionElement = React.ReactElement<typeof ToastAction>
+type ToastActionElement = React.ReactElement<typeof ToastAction>
 
 export {
   // type ToastProps,
