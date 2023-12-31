8b3fbe974210c8ae0f7727d5a5e4911fade29ac6
Akshai-M <mylapuramakshaikumar@gmail.com>
2023-12-31 23:21:16 +0530

    modified

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -148,45 +148,45 @@ function toast({ ...props }: Toast) {
       type: "UPDATE_TOAST",
       toast: { ...props, id },
     })
-//   const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id })
+  const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id })
 
-//   dispatch({
-//     type: "ADD_TOAST",
-//     toast: {
-//       ...props,
-//       id,
-//       open: true,
-//       onOpenChange: (open) => {
-//         if (!open) dismiss()
-//       },
-//     },
-//   })
+  dispatch({
+    type: "ADD_TOAST",
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open) => {
+        if (!open) dismiss()
+      },
+    },
+  })
 
-//   return {
-//     id: id,
-//     dismiss,
-//     update,
-//   }
-// }
+  return {
+    id: id,
+    dismiss,
+    update,
+  }
+}
 
-// function useToast() {
-//   const [state, setState] = React.useState<State>(memoryState)
+function useToast() {
+  const [state, setState] = React.useState<State>(memoryState)
 
-//   React.useEffect(() => {
-//     listeners.push(setState)
-//     return () => {
-//       const index = listeners.indexOf(setState)
-//       if (index > -1) {
-//         listeners.splice(index, 1)
-//       }
-//     }
-//   }, [state])
+  React.useEffect(() => {
+    listeners.push(setState)
+    return () => {
+      const index = listeners.indexOf(setState)
+      if (index > -1) {
+        listeners.splice(index, 1)
+      }
+    }
+  }, [state])
 
-//   return {
-//     ...state,
-//     toast,
-//     dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
-//   }
-// }
+  return {
+    ...state,
+    toast,
+    dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+  }
+}
 
 export { useToast, toast }
