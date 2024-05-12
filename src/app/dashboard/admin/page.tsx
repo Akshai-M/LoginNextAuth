358c005e44f7798a0b7c218003965252a66f1a92
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-05-12 20:02:46 +0530

    layout

---
+++ src/app/dashboard/admin/page.tsx
@@ -0,0 +1,21 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+
+const page = async () => {
+    const session = await getServerSession(authOptions);
+
+
+    if (session?.user) {
+        return (
+            <h2 className="text-2xl">Admin page {session?.user.username}
+            </h2>
+        );
+    }
+
+    return (
+        <h2 className="text-2xl">Please login</h2>
+    )
+};
+
+export default page;
